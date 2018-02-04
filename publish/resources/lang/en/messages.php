bc08d0e16b43cd2ebb96627c4c4dba701bce62a9
f4ndoq <32954710+f4ndoq@users.noreply.github.com>
2018-02-04 19:07:22 +0330

    Add files via upload

---
+++ publish/resources/lang/en/messages.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'addnewItem' => 'add new',
+    'delItem'=> [
+        'success'=> 'item deleted !',
+        'confirm' => 'are you sure to delete this item?',
+        'subconfirm' => 'Removed items are not refundable!'
+    ],
+    'search' => 'search...',
+    'searchTooltip' => 'type your text and enter',
+];
