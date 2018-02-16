ce34e29bdbe994f832ff80315b6567499412f71b
f4ndoq <32954710+f4ndoq@users.noreply.github.com>
2018-02-16 13:47:53 +0330

    Add files via upload

---
+++ publish/resources/views/layouts/adminNav.blade.php
@@ -0,0 +1,50 @@
+<nav id="sideNav"><!-- MAIN MENU -->
+    <ul class="nav nav-list">
+        <li>
+            <a class="dashboard" href="/admin">
+                <i class="main-icon fa fa-dashboard"></i> <span>Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="fa fa-menu-arrow pull-right"></i>
+                <i class="main-icon fa fa-bar-chart-o"></i> <span>level</span>
+            </a>
+            <ul><!-- submenus -->
+                <li><a href="graphs-flot.html">level 1</a></li>
+                <li>
+                    <a href="#">
+                        <i class="fa fa-menu-arrow pull-right"></i>
+                        level 2
+                    </a>
+                    <ul>
+                        <li><a href="#">test</a></li>
+                        <li><a href="#">test</a></li>
+                        <li><a href="#">test</a></li>
+                        <li><a href="#">test</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+        </li>
+
+        <li>
+            <a href="#">
+                <i class="main-icon fa fa-calendar"></i>
+                <span class="label label-warning pull-right">2</span> <span>Calendar</span>
+            </a>
+        </li>
+    </ul>
+
+    <!-- SECOND MAIN LIST -->
+    <h3>MORE</h3>
+    <ul class="nav nav-list">
+        <li>
+            <a href="#">
+                <i class="main-icon fa fa-link"></i>
+                <span class="label label-danger pull-right">PRO</span> <span>Frontend</span>
+            </a>
+        </li>
+    </ul>
+
+</nav>
